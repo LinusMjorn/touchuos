80d2eea43653916e7d73005709e9380d95533699
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-11-30 15:49:17 +0000

    added test for deleting bookmark and added delete function and path

--- Bookmarks/app.rb
+++ Bookmarks/app.rb
@@ -1,7 +1,11 @@
 require 'sinatra/base'
 require './lib/bookmark.rb'
 
+
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override
+
   get '/' do
     p ENV
     'Bookmark Manager'
@@ -21,7 +25,11 @@ class BookmarkManager < Sinatra::Base
     Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
-  
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
   
 
   run! if app_file == $0
